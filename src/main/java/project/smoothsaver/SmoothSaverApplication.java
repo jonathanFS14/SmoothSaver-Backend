ff374d5e3fdcc15c7c426756867f2b2516f0a10d
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-11-06 14:59:11 +0100

    initial commit

---
+++ src/main/java/project/smoothsaver/SmoothSaverApplication.java
@@ -0,0 +1,13 @@
+package project.smoothsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoothSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/smoothsaver/SmoothSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package project.smoothsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoothSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
