30c0540d4219e66fd703ee5141861bcf3c6f7a20
Mastore84 <76755222+Mastore84@users.noreply.github.com>
2023-11-19 16:46:52 +0100

    Update SmoothSaverApplicationTests.java

--- src/test/java/project/smoothsaver/SmoothSaverApplicationTests.java
+++ src/test/java/project/smoothsaver/SmoothSaverApplicationTests.java
@@ -3,10 +3,10 @@ package project.smoothsaver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SmoothSaverApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
