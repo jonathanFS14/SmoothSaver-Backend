2ecbd952ebd26b96558414a81a7f3c8dab33cf88
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-11-24 12:44:49 +0100

    tilføjet ting til at gemme store i databasen

--- src/main/java/project/smoothsaver/entity/SallingStore.java
+++ src/main/java/project/smoothsaver/entity/SallingStore.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import project.smoothsaver.dtos.SallingResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,18 +20,29 @@ public class SallingStore {
     private StoreAddress address;
     private String brand;
     @OneToMany(mappedBy = "sallingStore", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
     private List<StoreHours> hours;
     private String name;
     @Id
     private String id;
     private String type;
     @OneToMany(mappedBy = "sallingStore", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
     private List<ItemOnSale> clearances;
 
     @Getter
     @Setter
     @Entity
+    @NoArgsConstructor
     public static class StoreAddress {
+
+        public StoreAddress (String city, String country, String extra, String street, String zip){
+        this.city = city;
+        this.country = country;
+        this.extra = extra;
+        this.street = street;
+        this.zip = zip;
+        }
 
         @Id
         @Column(name = "sallingStore_id")
@@ -47,11 +59,21 @@ public class SallingStore {
         private String street;
         private String zip;
 
+
     }
     @Getter
     @Setter
     @Entity
+    @NoArgsConstructor
     public static class StoreHours {
+
+        public StoreHours (String date, String type, LocalDateTime open, LocalDateTime close, boolean closed){
+            this.date = date;
+            this.type = type;
+            this.open = open;
+            this.close = close;
+            this.closed = closed;
+        }
 
         @Id
         @Column(name = "sallingStore_id")
@@ -74,7 +96,24 @@ public class SallingStore {
     @Getter
     @Setter
     @Entity
+    @NoArgsConstructor
     public static class ItemOnSale {
+
+        public ItemOnSale (String currency, double discount, String ean, LocalDateTime endTime, LocalDateTime lastUpdate, double newPrice, double originalPrice, double percentDiscount, LocalDateTime startTime, int stock, String stockUnit, String description, String image){
+            this.currency = currency;
+            this.discount = discount;
+            this.ean = ean;
+            this.endTime = endTime;
+            this.lastUpdate = lastUpdate;
+            this.newPrice = newPrice;
+            this.originalPrice = originalPrice;
+            this.percentDiscount = percentDiscount;
+            this.startTime = startTime;
+            this.stock = stock;
+            this.stockUnit = stockUnit;
+            this.description = description;
+            this.image = image;
+        }
 
         @Id
         @Column(name = "sallingStore_id")
@@ -103,4 +142,12 @@ public class SallingStore {
         private String image;
     }
 
+    public SallingStore (SallingResponse response){
+        this.address = new StoreAddress(response.getStore().getAddress().getCity(), response.getStore().getAddress().getCountry(), response.getStore().getAddress().getExtra(), response.getStore().getAddress().getStreet(), response.getStore().getAddress().getZip());
+        this.brand = response.getStore().getBrand();
+        this.hours = response.getStore().getHours().stream().map(c -> new StoreHours(c.getDate(), c.getType(), c.getOpen(), c.getClose(), c.isClosed())).toList();
+        this.name = response.getStore().getName();
+        this.type = response.getStore().getType();
+        this.clearances = response.getClearances().stream().map(c -> new ItemOnSale(c.getOffer().getCurrency(), c.getOffer().getDiscount(), c.getOffer().getEan(), c.getOffer().getEndTime(), c.getOffer().getLastUpdate(), c.getOffer().getNewPrice(), c.getOffer().getOriginalPrice(), c.getOffer().getPercentDiscount(), c.getOffer().getStartTime(), c.getOffer().getStock(), c.getOffer().getStockUnit(), c.getProduct().getDescription(), c.getProduct().getImage())).toList();
+    }
 }
