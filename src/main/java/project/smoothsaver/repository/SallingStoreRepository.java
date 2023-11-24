6c0503383fe44672a6cadd0da7a85af072744ca6
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-11-24 10:32:39 +0100

    har lavet sallingStore entiteten som skal mappes ned til databasen

---
+++ src/main/java/project/smoothsaver/repository/SallingStoreRepository.java
@@ -0,0 +1,8 @@
+package project.smoothsaver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.smoothsaver.entity.SallingStore;
+
+public interface SallingStoreRepository extends JpaRepository<SallingStore,String> {
+
+}
